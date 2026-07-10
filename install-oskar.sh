551d2ab56d77fc1d67d1e3fe824a6a2cfdcbda92
Oliviax727 <oliviahrwalters@gmail.com>
2026-07-10 17:17:26 +0800

    Apply clean snapshot onto main for compare/merge

--- install-oskar.sh
+++
@@ -1,8 +0,0 @@
-OSKAR_REPO_DIR=$1
-OSKAR_OUT_DIR=$2
-
-cd $OSKAR_REPO_DIR
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$OSKAR_OUT_DIR -DFIND_OPENCL=ON -DBUILD_INFO=ON -DOpenCL_INCLUDE_DIR=/software/setonix/rocm/5.7.3/include/ -DOpenCL_LIBRARY=/software/setonix/rocm/5.7.3/opencl/lib/libOpenCL.so ../
-make -j8
-make install
--- smart_BTA_clean.sh
+++
@@ -1,9 +0,0 @@
-rm -r BTA/.nv
-rm -r BTA/output
-rm -r BTA/yuxiang1_fits
-rm -r BTA/telescope_model
-rm BTA/analysis.h5
-rm BTA/imager_template.ini
-rm BTA/interferometer_template.ini
-rm BTA/test_img.ini
-rm BTA/test_intif.ini
