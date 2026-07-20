416cc508692d1143fd3176ae858f695907f70a38
Oliviax727 <oliviahrwalters@gmail.com>
2026-07-20 15:31:45 +0800

    Shellchecked chips run script

--- oskar-chips/chips_run.sh
+++ oskar-chips/chips_run.sh
@@ -1,29 +1,51 @@
 #!/bin/bash -l
-#SBATCH --job-name=chips_ska
+#SBATCH --job-name=ohrw_chips_ska
 #SBATCH --output=chips.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=40
-#SBATCH --time=03:00:00
+#SBATCH --time=04:00:00
 #SBATCH --partition=mwa-gpu
 #SBATCH --account=mwaeor-gpu
 #SBATCH --export=NONE
-#SBATCH --gres=gpu:5
+#SBATCH --gres=gpu:8
 
 module use /software/projects/mwaeor/setonix/2024.05/modules/zen3/gcc/12.2.0
+# shellcheck disable=SC1091
 source /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/env_variables_garrawarla.sh
 module load cfitsio/4.4.0
 
 export OUTPUTDIR='/scratch/mwaeor/ohrw/chips_run_stage/'
 export INPUTDIR='/scratch/mwaeor/ohrw/chips_run_stage/'
 
-cd $INPUTDIR
+cd $INPUTDIR || exit
 
-/software/projects/mwaeor/ctrott/setonix/chips_2025_ska/gridvisska /scratch/mwaeor/ohrw/uvfits_templates/slice_uvwplane.uvfits side
+rm -rf ../chips_run_stage/*
+: > ../chips.out
 
-/software/projects/mwaeor/ctrott/setonix/chips_2025_ska/prepare_ska side 375 0 'yy' side 1
+mkdir -p ../2dps_png_templates
+rm -rf ../2dps_png_templates/*
 
-/software/projects/mwaeor/ctrott/setonix/chips_2025_ska/lssa_fg_ska side 375 50 'yy' 500. ps.chips 0 1
+mkdir -p ../2dps_npz_templates
+rm -rf ../2dps_npz_templates/*
 
+chips_files=(/scratch/mwaeor/ohrw/uvfits_templates/uvfits_templates/*)
+
+# shellcheck disable=SC1091
 source /software/projects/mwaeor/ohrw/.venv/bin/activate
 
-plotchips_all.py --basedir . --plot_type 2D --chips_tag ps.chips --polarisation YY --N_chan 375 --N_kperp 50 --max_power 1e14 --min_power 1e4 >> chips.log
+for template_file in "${chips_files[@]}"; do
+
+    template_name="$(awk -v s="${template_file}" 'BEGIN { start = 39; end = index(s, "_uvw_plane.uvfits") - start; print substr(s, start, end) }')"
+
+    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/gridvisska /scratch/mwaeor/ohrw/uvfits_templates/column_uvw_plane.uvfits side 15. 299792458.
+
+    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/prepare_ska side 100 0 'yy' side 1
+
+    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/lssa_fg_ska side 100 50 'yy' 500. chips.out 0 1
+
+    plotchips_all.py --basedir . --plot_type 2D --chips_tag chips.out --polarisation YY --N_chan 100 --N_kperp 50 --max_power 1e10 --min_power 1e3 >> chips.log
+
+    cp ./chips2D_yy_chips.out_crosspower.png ../2dps_png_templates/"${template_name}".png
+    cp ./2D_coords_and_power.npz ../2dps_npz_templates/"${template_name}".npz
+
+done
