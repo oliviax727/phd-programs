b66d076a50e921599949427a8736ba19997a9cda
Oliviax727 <oliviahrwalters@gmail.com>
2026-07-24 16:15:52 +0800

    updated testing.py to run flats

--- testing.py
+++ testing.py
@@ -5,21 +5,11 @@ Testing stage for OSKAR Regrid.
 
 # pylint: disable=line-too-long, unused-import, trailing-newlines
 from matplotlib import pyplot as plt
-from oskareor.oskar_exec import LoadDefaults, BTAnalysisPipeline
+from oskareor.oskar_exec import LoadDefaults as ldd, BTAnalysisPipeline as btap
 from oskareor.oskar_helpers import OSKARHelper as ohelp
 
 # Testing stage
 
-TEMPLATE = "flat"
+TEMPLATES = { "flat400" }
 
-BTAnalysisPipeline.run_oskar_on_model(
-    template_preset=TEMPLATE,
-    outpath=(
-        ohelp.default_template_path(template_preset=TEMPLATE, file_type="ms"),
-        ohelp.default_template_path(template_preset=TEMPLATE, file_type="vis"),
-        ohelp.default_template_path(template_preset=TEMPLATE, file_type="fits"),
-        ohelp.default_template_path(template_preset=TEMPLATE, file_type="uvfits"),
-    ),
-    oskar_mode="binary",
-    oskar_exec="~" + ohelp.OSKAR_BIN,
-)
+ldd.reload_all(update_which_templates=TEMPLATES)
