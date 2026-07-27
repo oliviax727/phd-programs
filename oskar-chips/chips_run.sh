162a2af6d9bc4330a81d64b6b4380f764d696585
Olivia Walters <oliviahrwalters@gmail.com>
2026-07-27 16:05:05 +0800

    Channel no

--- oskar-chips/chips_run.sh
+++ oskar-chips/chips_run.sh
@@ -14,6 +14,8 @@ module use /software/projects/mwaeor/setonix/2024.05/modules/zen3/gcc/12.2.0
 source /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/env_variables_garrawarla.sh
 module load cfitsio/4.4.0
 
+mkdir -p /scratch/mwaeor/ohrw/chips_run_stage/
+
 export OUTPUTDIR='/scratch/mwaeor/ohrw/chips_run_stage/'
 export INPUTDIR='/scratch/mwaeor/ohrw/chips_run_stage/'
 
@@ -30,20 +32,22 @@ rm -rf ../2dps_npz_templates/*
 
 chips_files=(/scratch/mwaeor/ohrw/active_uvfits/*)
 
+nchan=400
+
 for template_file in "${chips_files[@]}"; do
 
     template_name="$(awk -v s="${template_file}" 'BEGIN { start = 39; end = index(s, "_uvw_plane.uvfits") - start; print substr(s, start, end) }')"
 
     /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/gridvisska "${template_file}" side 15. 299792458.
 
-    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/prepare_ska side 100 0 'yy' side 1
+    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/prepare_ska side $nchan 0 'yy' side 1
 
-    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/lssa_fg_ska side 100 50 'yy' 500. chips.out 0 1
+    /software/projects/mwaeor/ctrott/setonix/chips_2025_ska/lssa_fg_ska side $nchan 50 'yy' 500. chips.out 0 1
 
     # shellcheck disable=SC1091
     source /software/projects/mwaeor/ohrw/.venv/bin/activate
 
-    plotchips_all.py --basedir . --plot_type 2D --chips_tag chips.out --polarisation YY --N_chan 100 --N_kperp 50 --max_power 1e10 --min_power 1e3 >> chips.log
+    plotchips_all.py --basedir . --plot_type 2D --chips_tag chips.out --polarisation YY --N_chan $nchan --N_kperp 100 --max_power 1e12 --min_power 1e5 >> chips.log
 
     deactivate
 
