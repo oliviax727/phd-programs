88e34fadbc2b68a8f37fda63c0b6d64eb47cc770
Oliviax727 <oliviahrwalters@gmail.com>
2026-08-14 15:11:54 +1000

    ran 21cmFast

---
+++ simulations/21cmfast_exec.py
@@ -0,0 +1,42 @@
+#!.venv/bin/python
+
+import py21cmfast as p21c
+from tempfile import mkdtemp
+
+print(f' Using 21cmFAST version {p21c.__version__}')
+
+scin_inputs = p21c.InputParameters.from_template(
+    ["Park19", "large"],
+    random_seed = 20250303,
+    HII_DIM = 512,
+    LOWRES_CELL_SIZE_MPC = 2,
+    N_THREADS = 8,
+    USE_CMB_HEATING = True,
+    USE_LYA_HEATING = True,
+    F_STAR10 = -1.42,
+    ALPHA_STAR = 0.51,
+    F_ESC10 = -1.08,
+    ALPHA_ESC = -0.46,
+    t_STAR = 0.34,
+    M_TURN = 8.46
+)
+
+scin_toml_file = "/fred/oz113/owalters/phd-programs/simulations/scin-park19.toml"
+
+p21c.write_template(scin_inputs, scin_toml_file)
+
+cache = p21c.OutputCache(mkdtemp())
+
+lcn = p21c.RectilinearLightconer.between_redshifts(
+    min_redshift=5.5,
+    max_redshift=12.0,
+    quantities=("brightness_temp", "density"),
+    resolution=scin_inputs.simulation_options.cell_size
+)
+
+lightcone = p21c.run_lightcone(
+    lightconer=lcn,
+    inputs=scin_inputs,
+    cache=cache,
+    progressbar=True
+)
--- simulations/run_21cmFast_slurm.sh
+++ simulations/run_21cmFast_slurm.sh
@@ -3,6 +3,16 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=700GB
 #SBATCH --time=12:00:00
-#SBATCH --job-name=gpc-box-21cmfast-test-olivia
+#SBATCH --job-name=gpc-py21c-olivia
 #SBATCH --account=oz113
-#SBATCH --chdir=/fred/oz113/owalters
+#SBATCH --output=/fred/oz113/owalters/slurm.out
+
+source /home/owalters/module-load.sh
+
+export GSL_LIB="/fred/oz113/owalters/gsl-2.8/lib"
+export GSL_INC="/fred/oz113/owalters/gsl-2.8/include"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/fred/oz113/owalters/gsl-2.8/lib"
+
+cd /fred/oz113/owalters/phd-programs
+
+./simulations/21cmfast_exec.py
